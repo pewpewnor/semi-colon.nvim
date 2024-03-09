d0bd13028e304e903a96854e0580a65fdc8ba319
pewpewnor <newpatchnotworking2@tutanota.com>
2024-03-09 23:01:23 +0700

    Revert "update"
    This reverts commit 3d65d06fd140b49db3cc1de5d188abe24506edba.

--- lua/tree-setter.lua
+++ lua/tree-setter.lua
@@ -4,7 +4,7 @@ local M = {}
 
 function M.init()
     require("nvim-treesitter").define_modules({
-        auto_semi_colon = {
+        tree_setter = {
             -- the file with the "main-code" of the module
             module_path = "tree-setter.main",
 
