11167b361ed96649bcd8956ff0628bc3b2a3c839
pewpewnor <newpatchnotworking2@tutanota.com>
2024-03-09 22:38:15 +0700

    update

--- lua/tree-setter.lua
+++ lua/tree-setter.lua
@@ -4,7 +4,7 @@ local M = {}
 
 function M.init()
     require("nvim-treesitter").define_modules({
-        tree_setter = {
+        semi_colon = {
             -- the file with the "main-code" of the module
             module_path = "tree-setter.main",
 
